f60fa0e67ccf0c7def897b16b79bef36f328a476
criftore <taufikboy392@gmail.com>
2026-06-02 21:41:44 +0700

    Clean Code

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-const cardContainer = document.querySelector(".card-container");
-const addBook = document.querySelector(".addBook");
-const dialog = document.getElementById("my-dialog");
-const bookForm = document.getElementById("bookForm");
-const cDialog = document.getElementById("closeDialog");
+// Dom Selection Start
 
+const cardContainer = document.querySelector(".cardContainer");
+const addButton = document.querySelector(".addButton");
+const myDialog = document.getElementById("myDialog");
+const containerForm = document.getElementById("containerForm");
+const closeDialog = document.getElementById("closeDialog");
+const formCover = document.getElementById("formCover");
+const formTitle = document.getElementById("formTitle").value;
+const formAuthor = document.getElementById("formAuthor").value;
+const formPages = document.getElementById("formPages").value;
+const formStatus = document.getElementById("formStatus").value;
+
+// Dom Selection End
+
+// Book Constructor Start
 function Book(title, author, pages, read, cover) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.cover = cover || "./c-logo.png";
+  this.cover = cover || "./c.logo.png";
   this.uId = crypto.randomUUID();
   this.info = console.log(
     `${this.title} by ${this.author} has ${this.pages} pages, ${this.read}`,
   );
 }
+// Book Constructor End
 
-Book.prototype.isRead = function () {
+// Book Prototype Start
+Book.prototype.readTogle = function () {
   if (this.read === "yes") {
     this.read = "no";
   } else {
@@ -24,20 +36,22 @@ Book.prototype.isRead = function () {
   }
 };
 
-Book.prototype.bookRead = function () {
+Book.prototype.readStatus = function () {
   if (this.read === "yes") {
     return {
       text: "Already Read It",
-      icon: `<img class="btnIcon" src="./eye-check-outline.svg">`,
+      icon: `<img class="buttonIcon" src="./eye-remove-outline.svg">`,
     };
   } else {
     return {
-      text: "Not Reading Yet",
-      icon: `<img class="btnIcon" src="./eye-remove-outline.svg">`,
+      text: "Not Reading It",
+      icon: `<img class="buttonIcon" src="./eye-check-outline.svg">`,
     };
   }
 };
+// Book Prototype End
 
+// myLibrary Array Start
 const myLibrary = [
   new Book(
     "Laskar Pelangi",
@@ -55,124 +69,121 @@ const myLibrary = [
     "./laut_bercerita_sampul.jpg",
   ),
 ];
+// myLibrary Array End
 
-function addBooktoLibrary(title, author, pages, read, cover) {
-  const nBook = new Book(title, author, pages, read, cover);
-  myLibrary.push(nBook);
-  return nBook;
+console.table(myLibrary);
+
+// Function Store Book Start
+function storeBook(title, author, pages, read, cover) {
+  const newBook = new Book(title, author, pages, read, cover);
+  myLibrary.push(newBook);
+  return newBook;
 }
+// Function Store Book End
 
-let displayLibrary = function (event) {
-  console.table(event);
-};
-
-displayLibrary(myLibrary);
-
-function render() {
+// Function Web Render and DOM Manipulation Start
+function renderWeb() {
   cardContainer.innerHTML = "";
 
   for (let i = 0; i < myLibrary.length; i++) {
+    // Create HTML element and its Class
     const cardContent = document.createElement("div");
     cardContent.className = "cardContent";
-    cardContainer.appendChild(cardContent);
-
     const bookCover = document.createElement("img");
     bookCover.className = "bookCover";
-    cardContent.appendChild(bookCover);
     bookCover.src = myLibrary[i].cover;
-
     const bookTitle = document.createElement("div");
     bookTitle.className = "bookTitle";
     bookTitle.innerText = myLibrary[i].title;
-    cardContent.appendChild(bookTitle);
-
     const bookAuthor = document.createElement("div");
     bookAuthor.className = "bookAuthor";
     bookAuthor.innerText = myLibrary[i].author;
-    cardContent.appendChild(bookAuthor);
-
     const bookPages = document.createElement("div");
     bookPages.className = "bookPages";
     bookPages.innerText = myLibrary[i].pages + " pages";
-    cardContent.appendChild(bookPages);
 
+    // Put Elemen On HTML
+    cardContainer.appendChild(cardContent);
+    cardContent.append(bookCover, bookTitle, bookAuthor, bookPages);
+
+    // Initialize prototype readStatus
+    const readStatus = myLibrary[i].readStatus();
+    const statusBook = document.createElement("div");
+    statusBook.className = "statusBook";
+    statusBook.innerText = readStatus.text;
+    const buttonBook = document.createElement("div");
+    buttonBook.className = "buttonBook";
+    cardContent.append(statusBook, buttonBook);
+
+    // Button Book
+    const statusButton = document.createElement("button");
+    statusButton.className = "statusButton";
+    statusButton.innerHTML = readStatus.icon;
+    const deleteButton = document.createElement("button");
+    deleteButton.className = "deleteButton";
+    deleteButton.innerHTML = `<img class="buttonIcon" src="./trash-can-outline.svg">`;
+    buttonBook.append(statusButton, deleteButton);
+
+    // Logic For uId
     if (!myLibrary[i].uId) {
       myLibrary[i].uId = crypto.randomUUID();
     }
     cardContent.dataset.id = myLibrary[i].uId;
 
-    const actionBtn = document.createElement("div");
-    actionBtn.className = "actionBtn";
-    const statusBook = document.createElement("div");
-    statusBook.className = "statusBook";
-    const statusBtn = document.createElement("button");
-    statusBtn.className = "statusBtn";
-
-    const statusRead = myLibrary[i].bookRead();
-
-    statusBook.innerText = statusRead.text;
-    statusBtn.innerHTML = statusRead.icon;
-
-    cardContent.appendChild(statusBook);
-    cardContent.appendChild(actionBtn);
-    actionBtn.appendChild(statusBtn);
-
-    const deleteBtn = document.createElement("button");
-    deleteBtn.className = "deleteBtn";
-    deleteBtn.innerHTML = `<img class="btnIcon" src="./trash-can-outline.svg"/>`;
-    actionBtn.appendChild(deleteBtn);
-
     console.log("Buku ke-" + (i + 1) + " adalah " + myLibrary[i].title);
   }
 }
 
-render();
+renderWeb();
+// Function Web Render and DOM Manipulation End
 
-bookForm.addEventListener("submit", function (event) {
+// Button Event Start
+// CardContainer Button
+cardContainer.addEventListener("click", (event) => {
+  // Select button By id target
+  const card = event.target.closest(".cardContent");
+  if (!card) return;
+  const targetId = card.dataset.id;
+  const targetIndex = myLibrary.findIndex((book) => book.uId === targetId);
+
+  // Delete Button
+  if (event.target.classList.contains("deleteButton")) {
+    if (targetIndex !== -1) myLibrary.splice(targetIndex, 1);
+    card.remove();
+  }
+  //   Toggel Read Button
+  if (event.target.classList.contains("statusButton")) {
+    if (targetIndex !== -1) {
+      myLibrary[targetIndex].readTogle();
+      renderWeb();
+    }
+  }
+});
+
+// Container Button
+containerForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  const bTitle = document.getElementById("bookTitle").value;
-  const bAuthor = document.getElementById("bookAuthor").value;
-  const bPages = document.getElementById("bookPages").value;
-  const bStatus = document.getElementById("bookStatus").value;
-  const bCover = document.getElementById("bookCover");
-  const fileCover = bCover.files[0];
 
+  //  Cover Default And Generator
+  const fileCover = formCover.files[0];
   let endCover = "./c-logo.png";
-
   if (fileCover) {
     if (!fileCover.type.startsWith("image/")) {
-      alert("Eror: Hanya file gambar (.jpg, .png, .webp) yang diijinkan!");
-      bCover.value = "";
+      alert("Error: Hanya file gambar (.jpg, .png, .webp) yang dibolehkan!");
+      formCover.value = "";
       return;
     }
     endCover = URL.createObjectURL(fileCover);
   }
-
-  addBooktoLibrary(bTitle, bAuthor, bPages, bStatus, endCover);
-  render();
-  bookForm.reset();
-  dialog.close();
+  //Update Function for Store Book
+  storeBook(formTitle, formAuthor, formPages, formStatus, endCover);
+  renderWeb();
+  containerForm.reset();
+  myDialog.closest();
+});
+// Close Dialog Button
+closeDialog.addEventListener("click", () => {
+  myDialog.close();
 });
 
-cDialog.addEventListener("click", () => {
-  dialog.close();
-});
-
-cardContainer.addEventListener("click", (event) => {
-  const card = event.target.closest(".cardContent");
-  if (!card) return;
-
-  const targetId = card.dataset.id;
-  const libraryIndex = myLibrary.findIndex((book) => book.uId === targetId);
-
-  if (event.target.classList.contains("deleteBtn")) {
-    if (libraryIndex !== -1) myLibrary.splice(libraryIndex, 1);
-    card.remove();
-  }
-  if (event.target.classList.contains("statusBtn")) {
-    if (libraryIndex !== -1) {
-      myLibrary[libraryIndex].isRead();
-      render();
-    }
-  }
-});
+// Button Event End
